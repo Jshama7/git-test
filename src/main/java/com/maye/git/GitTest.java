6e705fd4690a3f4f4163c66a5155c1388375f886
Jshama <1934130065@qq.com>
2023-02-25 15:42:26 +0800

    second commit

--- src/main/java/com/maye/git/GitTest.java
+++ src/main/java/com/maye/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("Hello,Git in Idea!");
+        System.out.println("Hello,Git in Idea 2!");
     }
 
 }
