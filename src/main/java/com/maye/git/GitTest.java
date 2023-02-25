444fd7be8a830d73b22e52f56a3c90cbe6a885c8
Jshama <1934130065@qq.com>
2023-02-25 15:39:32 +0800

    first commit

---
+++ src/main/java/com/maye/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.maye.git;
+
+/**
+ * @author Jshama
+ * @date 2023/2/25 15:35
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,Git in Idea!");
+    }
+
+}
